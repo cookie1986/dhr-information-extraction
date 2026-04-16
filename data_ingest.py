58367abf6c003358e6292cbb775a55a03134b682
cookie1986 <cookd.liv@gmail.com>
2026-04-16 16:29:05 +0100

    Add parse_dhr_results and extract_data_from_section functions

--- data_ingest.py
+++ data_ingest.py
@@ -1,6 +1,14 @@
 from src.fetch_results import fetch_html_content
+from src.parse_fetch_results import parse_dhr_results, extract_data_from_section
 
 # Fetch html content from Home Office library
 url = "https://homicide-review.homeoffice.gov.uk/?pagination[pageNumber]=0"
 
+# Fetch HTML content
 html_content = fetch_html_content(url=url, timeout=3, save_html=True)
+
+# Parse DHR results
+dhr_results = parse_dhr_results(html_content=html_content)
+
+# Extract data from DHR results sections
+structured_data_from_html = extract_data_from_section(dhr_results=dhr_results, output_dir="data/interim/", print_output=None)
---
+++ src/parse_fetch_results.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# CSS selectors for parsing DHR results page
+results_selector = "section[aria-label^='DHR result']"
+subsection_selector = ".govuk-grid-row.govuk-\!-padding-top-2 .govuk-grid-column-one-quarter .govuk-label.dhrr-results-card--detail-value"
+title_selector = "h3.govuk-heading-s"
+download_selector = ".govuk-grid-row.govuk-\!-padding-top-2 .govuk-grid-column-full .govuk-button-group.govuk-\!-margin-0 a[href^='/download/']"
+
+# Function to parse DHR results page
+def parse_dhr_results(html_content: str):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    dhr_results = soup.select(results_selector)
+
+    return dhr_results
+
+
+# Function to extract data from DHR results sections
+def extract_data_from_section(dhr_results, output_dir: Path = "data/interim/", print_output: str = None):
+    
+    # Initialise empty dataframe to hold extracted data
+    df = pd.DataFrame(columns=["title", "csp", "region", "upload_date", "death_date", "download_id"])
+
+    # Loop through DHR result in HTML and extract relevant data
+    for section in dhr_results:
+        
+        # Empty list to hold data extracted from HTML
+        extracted_data = []
+
+        # Extract title
+        title_element = section.select_one(title_selector)
+        title = " ".join(s.strip() for s in title_element.find_all(string=True, recursive=False) if s.strip()) if title_element else "N/A"
+
+        # Extract CSP, region, date of upload, date of death
+        dhr_subsections = section.select(subsection_selector)
+        csp = dhr_subsections[0].get_text().strip() if dhr_subsections else "N/A"
+
+        csp_region = dhr_subsections[1].get_text().strip() if len(dhr_subsections) > 1 else "N/A"
+        upload_date = dhr_subsections[2].get_text().strip() if len(dhr_subsections) > 2 else "N/A"
+        death_date = dhr_subsections[3].get_text().strip() if len(dhr_subsections) > 3 else "N/A"
+
+        # Extract the download ID
+        download_tag = section.select_one(download_selector)['href'] if section.select_one(download_selector) else None
+        download_id = download_tag.split('/')[-1] if download_tag else "N/A"
+
+        # Append extracted data to list
+        extracted_data.append({
+            "title": title,
+            "csp": csp,
+            "region": csp_region,
+            "upload_date": upload_date,
+            "death_date": death_date,
+            "download_id": download_id
+        })
+
+        # Auditing
+        if print_output == 'verbose':
+            print(f"Title: {title}")
+            print(f"Community Service Partnership: {csp}")
+            print(f"Region: {csp_region}")
+            print(f"Upload Date: {upload_date}")
+            print(f"Death Date: {death_date}")
+            print(f"Download ID: {download_id}")
+            print("-------end of section-------")
+        elif print_output == 'title_only':
+            print(f"Title: {title}")
+        else:
+            pass
+
+        # Add extracted data to dataframe
+        df = pd.concat([df, pd.DataFrame(extracted_data)], ignore_index=True)
+    # Write dataframe to CSV
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    df.to_csv(output_dir / "dhr_results.csv", index=False)
+
+    return df
