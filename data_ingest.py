78543ad35ca5fc00734cb0372b1af184b3b544ea
cookie1986 <cookd.liv@gmail.com>
2026-04-17 15:52:32 +0100

    Extract html content for all reports

--- data_ingest.py
+++ data_ingest.py
@@ -7,15 +7,13 @@ PAGE_SIZE = 100
 
 # Pagination testing
 total_reports = calculate_total_reports(base_url=BASE)
-print(f"total reports: {total_reports}")
 total_pages = calculate_total_pages(total_reports, PAGE_SIZE)
-print(f"total pages: {total_pages}")
 
-# Fetch HTML content
-html_content = fetch_html_content(url=BASE, page_count=total_pages, timeout=3, save_html=True)
+# Fetch HTML content (returns list of html content for each page)
+html_content_list = fetch_html_content(url=BASE, page_count=total_pages, timeout=3, save_html=True)
 
-# # Parse DHR results
-# dhr_results = parse_dhr_results(html_content=html_content)
+# Parse DHR results (returns list)
+dhr_results = parse_dhr_results(html_content_list=html_content_list)
 
 # # Extract data from DHR results sections
-# structured_data_from_html = extract_data_from_section(dhr_results=dhr_results, output_dir="data/interim/", print_output=None)
+structured_data_from_html = extract_data_from_section(dhr_results=dhr_results, output_dir="data/interim/", print_output=None)
--- src/parse_fetch_results.py
+++ src/parse_fetch_results.py
@@ -9,31 +9,34 @@ title_selector = "h3.govuk-heading-s"
 download_selector = ".govuk-grid-row.govuk-\!-padding-top-2 .govuk-grid-column-full .govuk-button-group.govuk-\!-margin-0 a[href^='/download/']"
 
 # Function to parse DHR results page
-def parse_dhr_results(html_content: str):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    dhr_results = soup.select(results_selector)
+def parse_dhr_results(html_content_list: list):
+    dhr_results = []
+    for html_content in html_content_list:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        dhr_results.extend(soup.select(results_selector))
 
     return dhr_results
 
 
 # Function to extract data from DHR results sections
-def extract_data_from_section(dhr_results, output_dir: Path = "data/interim/", print_output: str = None):
+def extract_data_from_section(dhr_results: list, output_dir: Path = "data/interim/", print_output: str = None):
     
     # Initialise empty dataframe to hold extracted data
     df = pd.DataFrame(columns=["title", "csp", "region", "upload_date", "death_date", "download_id"])
-
+    
     # Loop through DHR result in HTML and extract relevant data
-    for section in dhr_results:
-        
+    
+    for dhr in dhr_results:
+    
         # Empty list to hold data extracted from HTML
         extracted_data = []
 
         # Extract title
-        title_element = section.select_one(title_selector)
+        title_element = dhr.select_one(title_selector)
         title = " ".join(s.strip() for s in title_element.find_all(string=True, recursive=False) if s.strip()) if title_element else "N/A"
 
         # Extract CSP, region, date of upload, date of death
-        dhr_subsections = section.select(subsection_selector)
+        dhr_subsections = dhr.select(subsection_selector)
         csp = dhr_subsections[0].get_text().strip() if dhr_subsections else "N/A"
 
         csp_region = dhr_subsections[1].get_text().strip() if len(dhr_subsections) > 1 else "N/A"
@@ -41,7 +44,7 @@ def extract_data_from_section(dhr_results, output_dir: Path = "data/interim/", p
         death_date = dhr_subsections[3].get_text().strip() if len(dhr_subsections) > 3 else "N/A"
 
         # Extract the download ID
-        download_tag = section.select_one(download_selector)['href'] if section.select_one(download_selector) else None
+        download_tag = dhr.select_one(download_selector)['href'] if dhr.select_one(download_selector) else None
         download_id = download_tag.split('/')[-1] if download_tag else "N/A"
 
         # Append extracted data to list
@@ -70,6 +73,7 @@ def extract_data_from_section(dhr_results, output_dir: Path = "data/interim/", p
 
         # Add extracted data to dataframe
         df = pd.concat([df, pd.DataFrame(extracted_data)], ignore_index=True)
+    
     # Write dataframe to CSV
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
