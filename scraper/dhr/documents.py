107db879922cbf8ef671a2abcd73ddac086c2c4e
cookie1986 <cookd.liv@gmail.com>
2026-07-02 16:53:35 +0100

    Add 2x files to extract tags and format tag_matrix

---
+++ scraper/dhr/documents.py
@@ -0,0 +1,13 @@
+# documents.py
+
+import pandas as pd
+from download.pdfs import download_pdf
+
+def download_pdfs(download_id_dir: str, output_dir: str):
+    df = pd.read_csv(download_id_dir)
+    download_ids = df['download_id'].tolist()
+    for download_id in download_ids:
+        download_pdf(
+            download_id=download_id,
+            output_path=output_dir
+        )
